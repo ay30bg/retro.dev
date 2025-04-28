8d1aa6f7abc39b90f8af3591d5c85656e68dae73
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-04-28 07:36:26 +0100

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,15 +5,21 @@ export const Projects = () => (
     <h3>My Projects</h3>
     <ul>
       <li>
-        <strong>Project 1</strong>: A cool project built with React >>{' '}
-        <a href="https://github.com/yourusername/project1" style={{ color: '#0f0' }}>
+        <strong>Project 1</strong>: A website used for selling event tickets built with React, Node.js & Paystack APIs. >>{' '}
+        <a href="https://github.com/ay30bg/loudbox" style={{ color: '#0f0' }}>
           GitHub
         </a>
       </li>
       <li>
-        <strong>Project 2</strong>: Another awesome project >>{' '}
+        <strong>Project 2</strong>: An AI agent website Landing Page & Waitlist website built with React & Node.js. >>{' '}
         <a href="https://github.com/yourusername/project2" style={{ color: '#0f0' }}>
           GitHub
+        </a>
+      </li>
+      <li>
+        <strong>Project 3</strong>: Redesigned Bet9ja App and added new features. >>{' '}
+        <a href="https://www.behance.net/gallery/144820559/Bet9ja-Redesign" style={{ color: '#0f0' }}>
+          Behance
         </a>
       </li>
     </ul>
