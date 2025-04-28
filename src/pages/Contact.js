da9cd38609f075c7c9baab6b11cc22933f6160b2
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-04-28 07:15:05 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Contact = () => (
   <div>
     <h3>Contact Me</h3>
-    <p>Email: <a href="mailto:your.email@example.com" style={{ color: '#0f0' }}>dev.yekeen@gmail.com</a></p>
+    <p>Email: <a href="mailto:dev.yekeen@gmail.com" style={{ color: '#0f0' }}>dev.yekeen@gmail.com</a></p>
     <p>X: <a href="https://x.com/retrodevelop?s=21&t=qEPU6VWSzF-JHZscWE36Ug" style={{ color: '#0f0' }}>x.com/retro.dev</a></p>
     <p>GitHub: <a href="https://github.com/ay30bg" style={{ color: '#0f0' }}>github.com/ay30bg</a></p>
   </div>
