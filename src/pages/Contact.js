e43f9fda5ab0146b90f56f2b942830e69a57303e
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-04-28 07:43:20 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,6 @@ export const Contact = () => (
     <p>Email: <a href="mailto:dev.yekeen@gmail.com" style={{ color: '#0f0' }}>dev.yekeen@gmail.com</a></p>
     <p>X: <a href="https://x.com/retrodevelop?s=21&t=qEPU6VWSzF-JHZscWE36Ug" style={{ color: '#0f0' }}>x.com/retro.dev</a></p>
     <p>GitHub: <a href="https://github.com/ay30bg" style={{ color: '#0f0' }}>github.com/ay30bg</a></p>
-  <p>GitHub: <a href="https://www.behance.net/yekeenolalekan" style={{ color: '#0f0' }}>behance.net/yekeenolalekan</a></p>
+  <p>Behance: <a href="https://www.behance.net/yekeenolalekan" style={{ color: '#0f0' }}>behance.net/yekeenolalekan</a></p>
   </div>
 );
